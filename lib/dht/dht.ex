d50fee651f3e1c67b433821047fccbec3b5907ab
Yhodhan <mscuderi855@gmail.com>
2026-09-14 19:20:53 +0200

    Add torrent manager to schedule worker triggers and announcements

--- lib/dht/dht.ex
+++ lib/dht/dht.ex
@@ -8,15 +8,12 @@ defmodule Exorrent.DHT do
   def new_node(),
     do: Exalia.new_node()
 
-  def bootstrap() do
-    {:ok, pid, id} = Exalia.bootstrap()
-
+  def bootstrap(pid, id) do
+    Exalia.bootstrap(pid, id)
     # check that the routing table is healthy enough
     amount_candidates = length(Exalia.get_contacts(pid))
 
     if amount_candidates < @min, do: lookup_contacts(pid, id, 3)
-
-    {:ok, pid, id}
   end
 
   def find_peers_and_connect(pid, torrent) do
@@ -24,14 +21,13 @@ defmodule Exorrent.DHT do
     peers = Exalia.get_peers(pid, torrent.info_hash)
 
     Logger.info("=== Peers Obtainied ===")
-    IO.inspect(peers, label: "peers")
 
     Tracker.init_workers(torrent, peers)
   end
 
-  def announce(torrent) do
+  def announce(pid, torrent) do
     port = Application.get_env(:exorrent, :torrent_port)
-    Exalia.announce_peer(torrent.info_hash, port)
+    Exalia.announce_peer(pid, torrent.info_hash, port)
   end
 
   # ------------------------
---
+++ lib/torrent_manager.ex
@@ -0,0 +1,72 @@
+defmodule Exalia.TorrentManager do
+  use GenServer
+
+  require Logger
+
+  @announce_interval :timer.minutes(5)
+  @workers_discovery :timer.minutes(5)
+
+  def start_link(torrent \\ %{}),
+    do: GenServer.start_link(__MODULE__, torrent)
+
+  def init(torrent) do
+    Logger.info("=== Init Torrent Manager ===")
+
+    send(self(), :init_peer_sources)
+
+    {:ok, %{torrent: torrent}}
+  end
+
+  def handle_info(:init_peer_sources, %{torrent: t} = state) do
+    Logger.info("=== Init Peer sources ===")
+
+    if t.urls != [], do: Exorrent.Webseed.handle_webseeds(t)
+    if t.trackers != [], do: Exorrent.Tracker.handle_trackers(t)
+
+    {:ok, pid} = bootstrap_dht(t)
+
+    schedule_dht_announce()
+    schedule_worker_discovery()
+
+    {:ok, %{state | pid: pid}}
+  end
+
+  def handle_info(:workers_discovery, %{pid: pid, torrent: t} = state) do
+    Exorrent.DHT.find_peers_and_connect(pid, t)
+
+    schedule_worker_discovery()
+
+    {:noreply, state}
+  end
+
+  def handle_info(:announce, %{pid: pid, torrent: t} = state) do
+    Exorrent.DHT.announce(pid, t)
+
+    schedule_dht_announce()
+
+    {:noreply, state}
+  end
+
+  # ---------------------------------
+  #          Bootstrap DHT
+  # ---------------------------------
+
+  def bootstrap_dht(t) do
+    {:ok, pid, id} = Exorrent.DHT.new_node()
+
+    Task.Supervisor.start_child(Exorrent.TaskSupervisor, fn ->
+      Exorrent.DHT.bootstrap(pid, id)
+
+      Exorrent.DHT.find_peers_and_connect(pid, t)
+      Exorrent.DHT.announce(pid, t)
+    end)
+
+    {:ok, pid}
+  end
+
+  def schedule_dht_announce(),
+    do: Process.send_after(self(), :announce, @announce_interval)
+
+  def schedule_worker_discovery(),
+    do: Process.send_after(self(), :init_workers, @workers_discovery)
+end
